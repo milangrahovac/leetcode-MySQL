4893fe4e6d410067be2caafdcebf4b9545cac270
Milan Grahovac <grahovac85@gmail.com>
2018-11-25 13:32:37 +0100

    add 196

---
+++ 196-delete_duplicate_emails.sql
@@ -0,0 +1,31 @@
+-- 196. Delete Duplicate Emails
+
+-- Write a SQL query to delete all duplicate email entries in a table named Person, 
+-- keeping only unique emails based on its smallest Id.
+
+-- +----+------------------+
+-- | Id | Email            |
+-- +----+------------------+
+-- | 1  | john@example.com |
+-- | 2  | bob@example.com  |
+-- | 3  | john@example.com |
+-- +----+------------------+
+
+-- Id is the primary key column for this table.
+-- For example, after running your query, the above Person table should have the following rows:
+
+-- +----+------------------+
+-- | Id | Email            |
+-- +----+------------------+
+-- | 1  | john@example.com |
+-- | 2  | bob@example.com  |
+-- +----+------------------+
+
+-- Note:
+-- Your output is the whole Person table after executing your sql. Use delete statement.
+
+
+DELETE p1 FROM Person p1
+        INNER JOIN Person p2
+WHERE 
+    p1.id > p2.id AND p1.Email = p2.Email;
